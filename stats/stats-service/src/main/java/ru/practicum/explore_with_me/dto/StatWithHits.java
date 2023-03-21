08e1212d850d0feaec36689c201f65b01be4c5b5
laserdi <v013v@ya.ru>
2023-03-21 23:26:38 +0200

    Diplom stage 1 after review №1. • fix all. • all job.

--- stats/stats-service/src/main/java/ru/practicum/explore_with_me/dto/StatWithHits.java
+++ stats/stats-service/src/main/java/ru/practicum/explore_with_me/dto/StatWithHits.java
@@ -6,7 +6,6 @@ import lombok.*;
 @Setter
 @RequiredArgsConstructor
 @AllArgsConstructor
-@Builder(toBuilder = true)
 public class StatWithHits {
     private String app;
     private String uri;
--- stats/stats-service/src/main/java/ru/practicum/explore_with_me/repositiry/StatRepository.java
+++ stats/stats-service/src/main/java/ru/practicum/explore_with_me/repositiry/StatRepository.java
@@ -20,7 +20,7 @@ public interface StatRepository extends JpaRepository<Stat, Long> {
     @Query("select new ru.practicum.explore_with_me.dto.StatWithHits(s.app.app, s.uri, count (distinct s.ip))" +
             "from Stat s " +
             "where s.timestamp between ?1 and ?2 " +
-            "group by s.app, s.uri " +
+            "group by s.app.app, s.uri " +
             "order by count (distinct s.ip) desc")
     List<StatWithHits> findAllUniqueWhenUriIsEmpty(LocalDateTime start, LocalDateTime end);
 
@@ -28,13 +28,13 @@ public interface StatRepository extends JpaRepository<Stat, Long> {
             + "from Stat s "
             + "where s.timestamp between ?1 and ?2 "
             + "and s.uri in (?3)"
-            + "group by s.app, s.uri "
+            + "group by s.app.app, s.uri "
             + "order by count (distinct s.ip) desc ")
     List<StatWithHits> findAllUniqueWhenUriIsNotEmpty(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("select new ru.practicum.explore_with_me.dto.StatWithHits(s.app.app, s.uri, count(s.ip)) "
             + "from Stat s where s.timestamp between ?1 and ?2 "
-            + " group by s.app, s.uri "
+            + " group by s.app.app, s.uri "
             + " order by count(s.ip) desc")
     List<StatWithHits> findAllWhenUriIsEmpty(LocalDateTime start, LocalDateTime end);
 
@@ -42,7 +42,7 @@ public interface StatRepository extends JpaRepository<Stat, Long> {
             + "from Stat s "
             + "where s.timestamp between ?1 and ?2 "
             + "and s.uri in (?3)"
-            + "group by s.app, s.uri "
+            + "group by s.app.app, s.uri "
             + "order by count (s.ip) desc")
     List<StatWithHits> findAllWhenStarEndUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 }
